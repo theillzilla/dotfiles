977e408d33b3cf7aa5c22d94a4fedc3478cbf0a5
Vincent Hilton <vincent.hilton@WDAPL009.local>
2020-11-05 13:28:50 -0500

    display script for mac

---
+++ display.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# run this command to setup display in mac
+displayplacer "id:772067C0-2CB9-1DE7-7877-15E152FE75D3 res:1920x1080 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0" "id:5DA908AD-E526-82CA-BA70-B36377A262C4 res:1440x900 color_depth:8 scaling:on origin:(1920,0) degree:0" "id:6D32359F-C97E-48BB-7B0E-47C083A69CAF res:1080x1920 hz:60 color_depth:8 scaling:off origin:(-1080,-460) degree:90"
